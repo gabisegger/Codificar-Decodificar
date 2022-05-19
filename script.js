d643e1664d00ccb376f3e0fa040c0e4381480007
gabisegger <gabi.segger@gmail.com>
2022-05-19 13:51:02 -0300

    feat: script Cesar Cipher and Base64

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //Botao codificar ou decodificar
-var btn = document.getElementById('btn')
-var radiocode = document.getElementById('radiocode')
-var radiodecode = document.getElementById('radiodecode')
+const btn = document.getElementById('btn')
+const radiocode = document.getElementById('radiocode')
+const radiodecode = document.getElementById('radiodecode')
 
 radiocode.addEventListener('click', () => {
   btn.innerText = 'Codificar'
@@ -13,12 +13,12 @@ radiodecode.addEventListener('click', () => {
 //FIM Botao codificar ou decodificar
 
 //Inserindo campo de passos quando a opção Cifra de César for selecionada
-var selectCode = document.getElementById('selectCode')
-var steps = document.getElementById('steps')
+const selectCode = document.getElementById('selectCode')
+const steps = document.getElementById('steps')
 
 selectCode.addEventListener('click', () => {
 
-  if (selectCode.value == 'cesar') {
+  if(selectCode.value == 'cesar'){
     steps.style.display = "flex";
     steps.classList.add('add')
   }else {
@@ -28,3 +28,65 @@ selectCode.addEventListener('click', () => {
 })
 //FIM Inserindo campo de passos quando a opção Cifra de César for selecionada
 
+const passos = document.getElementById('passos')
+const message = document.getElementById('text')
+const result = document.getElementById('result')
+
+//Cifra de césar - codificar
+const cifraDeCesar = (message, steps) => {
+  let encode = '';
+  let result;
+
+  for(let i = 0; i < message.length; i++){
+    if(message.charCodeAt(i) >= 65 && message.charCodeAt(i) <= 90){
+      result = (((message.charCodeAt(i) - 65) + steps) % 26) + 65;
+    }else if(message.charCodeAt(i) >= 97 && message.charCodeAt(i) <= 122){
+      result = (((message.charCodeAt(i) - 97) + steps) % 26) + 97;
+    }else if(message.charCodeAt(i) === 32){
+      result = 32;
+    }
+    encode += String.fromCharCode(result);
+  }
+  return encode.toLowerCase();
+}
+//FIM Cifra de césar - codificar
+
+//Cifra de césar - decodificar
+const decifraDeCesar = (message, steps) => {
+  let decode = '';
+  let result;
+
+  for(let i = 0; i < message.length; i++){
+    if(message.charCodeAt(i) >= 65 && message.charCodeAt(i) <= 90){
+      result = (((message.charCodeAt(i) - 90) - steps) % 26) + 90;
+    }else if(message.charCodeAt(i) >= 97 && message.charCodeAt(i) <= 122){
+      result = (((message.charCodeAt(i) - 122) - steps) % 26) + 122;
+    }else if(message.charCodeAt(i) === 32){
+      result = 32;
+    }
+    decode += String.fromCharCode(result);
+  }
+  return decode.toLowerCase();
+}
+//FIM Cifra de césar - decodificar
+
+btn.addEventListener('click', (e) => {
+  e.preventDefault()
+    //Cifra de cesar.
+    if(selectCode.value == 'cesar'){
+      if(radiocode.checked == true){
+        result.innerText = cifraDeCesar(message.value, Number(passos.value))
+      }else if(radiodecode.checked == true){
+        result.innerText = decifraDeCesar(message.value, Number(passos.value))
+      }}
+    //FIM Cifra de cesar
+    //Base64.
+    else if(selectCode.value == 'base64'){
+      if(radiocode.checked == true){
+        result.innerText = btoa(message.value)
+      }else if(radiodecode.checked == true){
+        result.innerText = atob(message.value)
+      }}
+    //FIM Base64
+    else return alert(`Escolha uma codificação`)
+  })
