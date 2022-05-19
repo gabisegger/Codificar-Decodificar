6fe9bc4812715f209985e6086d0a2e681dabf783
gabisegger <gabi.segger@gmail.com>
2022-05-19 10:18:12 -0300

    feat: insert steps field when select Cesar Cipher

--- script.js
+++ script.js
@@ -10,4 +10,21 @@ radiocode.addEventListener('click', () => {
 radiodecode.addEventListener('click', () => {
   btn.innerText = 'Decodificar'
 })
+//FIM Botao codificar ou decodificar
+
+//Inserindo campo de passos quando a opção Cifra de César for selecionada
+var selectCode = document.getElementById('selectCode')
+var steps = document.getElementById('steps')
+
+selectCode.addEventListener('click', () => {
+
+  if (selectCode.value == 'cesar') {
+    steps.style.display = "flex";
+    steps.classList.add('add')
+  }else {
+    steps.style.display = "none";
+  }
+
+})
+//FIM Inserindo campo de passos quando a opção Cifra de César for selecionada
 
